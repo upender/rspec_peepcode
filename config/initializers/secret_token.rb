d12ccc06e3d9a41ac57be92642345b5e7b3c2f28
upender <upender@jasita.com>
2011-08-20 22:32:02 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RspecPeepcode::Application.config.secret_token = 'ba9978b5d73f4906d5558a00a7af2dbdad3f0ec341d0db1bb14153c28c73b6c6a922b2c761532f0fb4fce88b9aae95c8c1469a8f6750f8566e5f1ab900eaabd8'
